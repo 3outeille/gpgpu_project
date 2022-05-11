87b0f03aa5a4067ef46748fa92bf24b9e6054b6a
Ferdinand Mom <ferdinandmom.business@gmail.com>
2022-05-11 14:44:39 +0200

    feat: cpu version white image

--- src/render_cpu.cpp
+++ src/render_cpu.cpp
@@ -1,8 +1,19 @@
 #include "render.hpp"
 #include <iostream>
 
-void render_cpu(char* buffer, int width, int height, std::ptrdiff_t stride, int n_iterations) 
+void render_cpu(char *buffer, int width, int height, std::ptrdiff_t stride, int n_iterations)
 {
     std::cout << "Hello" << std::endl;
+
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                buffer[i * stride + j * 4 + k] = 255;
+            }
+        }
+    }
     return;
 }
